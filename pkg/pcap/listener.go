07edf1ec036005ae1bc973b03a898ac1dadb92d1
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-26 21:00:10 +0200

    pcap: rename listener variables
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/pcap/listener.go
+++ pkg/pcap/listener.go
@@ -30,51 +30,51 @@ type Listener struct {
 }
 
 // getFirstPcapInterface sets the first network interface found by pcap
-func (p *Listener) getFirstPcapInterface() {
+func (l *Listener) getFirstPcapInterface() {
 	ifs, err := pcap.FindAllDevs()
 	if err != nil {
 		log.Fatal(err)
 	}
 	if len(ifs) > 0 {
-		p.Device = ifs[0].Name
+		l.Device = ifs[0].Name
 		return
 	}
 	log.Fatal("No network interface found")
 }
 
 // Prepare prepares the pcap listener for the listen function
-func (p *Listener) Prepare() {
+func (l *Listener) Prepare() {
 	// open pcap handle
 	var pcapErr error
 	var startText string
-	if p.File == "" {
+	if l.File == "" {
 		// set pcap timeout
 		timeout := pcap.BlockForever
-		if p.Timeout > 0 {
-			timeout = p.Timeout
+		if l.Timeout > 0 {
+			timeout = l.Timeout
 		}
 
 		// set interface
-		if p.Device == "" {
-			p.getFirstPcapInterface()
+		if l.Device == "" {
+			l.getFirstPcapInterface()
 		}
 
 		// open device
-		p.pcapHandle, pcapErr = pcap.OpenLive(p.Device,
-			int32(p.Snaplen), p.Promisc, timeout)
+		l.pcapHandle, pcapErr = pcap.OpenLive(l.Device,
+			int32(l.Snaplen), l.Promisc, timeout)
 		startText = fmt.Sprintf("Listening on interface %s:\n",
-			p.Device)
+			l.Device)
 	} else {
 		// open pcap file
-		p.pcapHandle, pcapErr = pcap.OpenOffline(p.File)
+		l.pcapHandle, pcapErr = pcap.OpenOffline(l.File)
 		startText = fmt.Sprintf("Reading packets from file %s:\n",
-			p.File)
+			l.File)
 	}
 	if pcapErr != nil {
 		log.Fatal(pcapErr)
 	}
-	if p.Filter != "" {
-		if err := p.pcapHandle.SetBPFFilter(p.Filter); err != nil {
+	if l.Filter != "" {
+		if err := l.pcapHandle.SetBPFFilter(l.Filter); err != nil {
 			log.Fatal(pcapErr)
 		}
 	}
@@ -82,29 +82,29 @@ func (p *Listener) Prepare() {
 }
 
 // Loop implements the listen loop for the listen function
-func (p *Listener) Loop() {
-	defer p.pcapHandle.Close()
+func (l *Listener) Loop() {
+	defer l.pcapHandle.Close()
 
 	// make sure there is a packet handler
-	if p.PacketHandler == nil {
+	if l.PacketHandler == nil {
 		log.Fatal("no packet handler set")
 	}
 
 	// Use the handle as a packet source to process all packets
-	packetSource := gopacket.NewPacketSource(p.pcapHandle,
-		p.pcapHandle.LinkType())
+	packetSource := gopacket.NewPacketSource(l.pcapHandle,
+		l.pcapHandle.LinkType())
 	packets := packetSource.Packets()
 
 	// setup timer and check timer handler
-	ticker := time.Tick(p.Timer)
-	if ticker != nil && p.TimerHandler == nil {
+	ticker := time.Tick(l.Timer)
+	if ticker != nil && l.TimerHandler == nil {
 		log.Fatal("timer used but no timer handler set")
 	}
 
 	// set stop time if configured
 	stop := make(<-chan time.Time)
-	if p.MaxTime > 0 {
-		stop = time.After(p.MaxTime)
+	if l.MaxTime > 0 {
+		stop = time.After(l.MaxTime)
 	}
 
 	// handle packets and timer events
@@ -115,13 +115,13 @@ func (p *Listener) Loop() {
 			if packet == nil {
 				return
 			}
-			p.PacketHandler.HandlePacket(packet)
+			l.PacketHandler.HandlePacket(packet)
 			count++
-			if p.MaxPkts > 0 && count == p.MaxPkts {
+			if l.MaxPkts > 0 && count == l.MaxPkts {
 				return
 			}
 		case <-ticker:
-			p.TimerHandler.HandleTimer()
+			l.TimerHandler.HandleTimer()
 		case <-stop:
 			return
 		}
